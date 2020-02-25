a899b9416c0f8e70461d244fab32eee4fe7955e5
shmuelko <shmuel777@gmail.com>
2020-02-25 15:48:03 +0200

    Add initial functions

--- src/test_xml_parser/core.clj
+++ src/test_xml_parser/core.clj
@@ -1,6 +1,18 @@
-(ns test-xml-parser.core)
+(ns test-xml-parser.core
+  (:require
+   [clojure.xml :as xml]
+   [clojure.zip :as zip]
+   [clojure.pprint :as pprint]
+   [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn zip-str [s]
+  (zip/xml-zip
+   (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
+
+(defn find-testcase-tag [xml-content]
+  (let [filter-result (filter #(= (:tag %) :testcase) xml-content)]
+    (if (empty? filter-result)
+      (if (contains? (first xml-content) :content)
+        (find-testcase-tag (:content (first xml-content)))
+        xml-content)
+      filter-result)))
