4ae098ab803e9fca3285a7c92c2b54de7b1f0530
shmuelko <shmuel777@gmail.com>
2020-02-25 15:40:37 +0200

    first commit

---
+++ test/test_xml_parser/core_test.clj
@@ -0,0 +1,7 @@
+(ns test-xml-parser.core-test
+  (:require [clojure.test :refer :all]
+            [test-xml-parser.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
