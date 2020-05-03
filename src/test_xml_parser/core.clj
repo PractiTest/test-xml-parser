a3d9f20e795f76b022fcd23b49ec1288bea50e4d
shmuelko <shmuel777@gmail.com>
2020-05-03 17:34:24 +0300

    :content

--- src/test_xml_parser/core.clj
+++ src/test_xml_parser/core.clj
@@ -32,8 +32,8 @@
         print   (pprint/pprint zip-val)
         ]
     (if (= (:tag (first zip-val)) :testsuites)
-      (group-testcase-data (filter-tags (zip/down zip-val) :testcase))
-      (group-testcase-data (filter-tags (zip/down zip-val) :testcase)))))
+      (group-testcase-data (filter-tags (:content zip-val) :testcase))
+      (group-testcase-data (filter-tags (:content zip-val) :testcase)))))
 
 (defn get-files-data [files]
   (let [grouped-files (for [file files] (get-data file))]
