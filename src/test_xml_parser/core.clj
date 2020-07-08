48aa5e59a26b3bd9fbbd3ff7b4f2beaead43459f
shmuelko <shmuel777@gmail.com>
2020-07-08 19:16:35 +0300

    display-files

--- src/test_xml_parser/core.clj
+++ src/test_xml_parser/core.clj
@@ -102,6 +102,15 @@
         ]
     files))
 
+(defn return-file [file]
+  (pprint/pprint (slurp file)))
+
+(defn return-files [directory]
+  (let [filtered-files   (filter (fn [file] (str/ends-with? (.getAbsolutePath file) ".xml")) (file-seq directory))
+        filtered-paths   (for [file filtered-files] (.getAbsolutePath file))
+        files            (for [path filtered-paths] (return-file path))]
+    files))
+
 (defn get-dir-by-path [path]
   (let [directory (clojure.java.io/file path)]
     (send-directory directory '("" "" ""))))
