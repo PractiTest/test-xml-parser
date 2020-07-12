d3d0b193b95c9df7a1a4cb7f1abd96ec7457ea16
shmuelko <shmuel777@gmail.com>
2020-07-12 18:53:13 +0300

    send-dir

--- src/test_xml_parser/core.clj
+++ src/test_xml_parser/core.clj
@@ -65,7 +65,7 @@
         filtered-paths   (for [file filtered-files] (.getAbsolutePath file))
         files            (for [path filtered-paths] (slurp path))
         [grouped-data]   (get-files-data files)
-        result           (doall (map parse-n-merge-data parsed-content grouped-data))]
+        result           (for [parsed parsed-content] (parse-n-merge-data grouped-data parsed))]
     result))
 
 (defn remove-bom [directory]
